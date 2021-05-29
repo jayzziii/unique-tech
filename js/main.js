ee1380ca206250d35edfc13339f34c904bb850b5
jayzziii <alvarez.juliancris@gmail.com>
2021-05-29 16:54:25 +0800

    Edit tabs from spaces 05292021

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$('.navbar-burger').click(function () {
-		$('.navbar-menu').toggleClass('is-active');
-	});
+  $(".navbar-burger").click(function () {
+    $(".navbar-menu").toggleClass("is-active");
+  });
 });
